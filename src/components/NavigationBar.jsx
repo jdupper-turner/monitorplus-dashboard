2def0b606241e3b03a892d1e5cc558ab751e1ccf
John Dupper <john.dupper@turner.com>
2021-12-18 10:22:31 -0700

    made navbar w/ current time

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,34 @@
+import { AppBar, Toolbar, Typography } from '@mui/material'
+import { AccessTime } from '@mui/icons-material'
+import { Fragment, useState } from 'react'
+import dayjs from 'dayjs'
+
+
+const NavigationBar = () => {
+   return (
+      <Fragment>
+         <AppBar position='static'>
+            <Toolbar>
+               <Typography sx={{ flexGrow: 1 }}>
+                  MonitorPlus Dashboard
+               </Typography>
+               <CurrentTime />
+            </Toolbar>
+         </AppBar>
+      </Fragment>
+   )
+}
+
+const CurrentTime = () => {
+   const [now, setNow] = useState(new Date())
+   const updateTime = () => setNow(new Date())
+   setTimeout(() => updateTime(), 1000)
+
+   return (
+      <Fragment>
+         <AccessTime /> &nbsp; {dayjs(now).format('dddd, MMMM DD YYYY, HH:mm:ss')}
+      </Fragment>
+   )
+}
+
+export default NavigationBar
